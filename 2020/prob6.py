cbc16fce268c1b0b1e44fd63029758f0d4f66e0a
Colin Sturm <drcsturm@gmail.com>
2020-12-06 14:32:46 -0600

    prob6 complete

--- 2020/prob6.py
+++ 2020/prob6.py
@@ -1,12 +1,21 @@
 with open("prob6_input.txt", 'r') as f:
     rawdata = f.readlines()
 
-part1 = 0
+part1_count = 0
+part2_count = 0
+part2 = None
 newdata = []
 for line in rawdata:
     if line == "\n":
-        part1 += len(set(newdata))
+        part1_count += len(set(newdata))
+        part2_count += len(part2)
+        part2 = None
         newdata = []
         continue
     newdata += [i for i in line.strip()]
-print(f"Prob 6 Part 1: {part1}")
+    if part2 is None:
+        part2 = set([i for i in line.strip()])
+    else:
+        part2 = part2.intersection(set([i for i in line.strip()]))
+print(f"Prob 6 Part 1: {part1_count}")
+print(f"Prob 6 Part 2: {part2_count}")
